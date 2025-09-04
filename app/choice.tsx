e53c1ff5708aef855ffbf25c15ee46c5222b8428
Donaldson10 <166642140+Donaldson10@users.noreply.github.com>
2025-09-04 12:32:24 -0700

    Create choice.tsx
    initia page setup

---
+++ app/choice.tsx
@@ -0,0 +1,8 @@
+import { useRouter } from 'expo-router';
+import { View } from 'react-native';
+import { Pressable, TextInput, Text } from 'react-native-gesture-handler';
+import { Platform, StyleSheet } from 'react-native';
+
+export default function choices(){
+    
+}

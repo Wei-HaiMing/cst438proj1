b5976ddd84b79e9d84612f4056978c9820801b1c
Wei-HaiMing <ezedito@gmail.com>
2025-09-10 19:34:47 -0700

    trying to update packages

---
+++ __tests__/HomeScreen-test.tsx
@@ -0,0 +1,11 @@
+// import { render } from '@testing-library/react-native';
+import HomeScreen, { CustomText } from '@/app/jesttest';
+
+describe('<HomeScreen />', () => {
+//   test('Text renders correctly on HomeScreen', () => {
+//     const { getByText } = render(<HomeScreen />);
+
+//     getByText('Welcome!');
+//   });
+    test('smoke', () => expect(true).toBe(true));
+});
